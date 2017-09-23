89fcf8981b20ca8b7f27ea1c0b542367c76603b9
Nastya <anastasia.vorton@gmail.com>
2017-09-23 16:57:20 +0300

    Task1 class added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Date;
+
 /**
  * Created by Nastya on 23.09.2017.
  */
 public class Main {
+    Date A = new Date(1998, 6, 20);
+    Date B = new Date(1998, 3, 17);
+    Date C = new Date(1998, 9, 28);
+    Date D = new Date(1999, 5, 27);
+
     public static void main(String[] args) {
     }
 }
--- src/Task1.java
+++ src/Task1.java
@@ -1,10 +1,12 @@
+import java.util.Date;
+
 /**
  * Created by Nastya on 23.09.2017.
  * Class for task number 1
  */
 public class Task1 {
-    int A = 19;
-    int B = 19;
-    int C = 18;
-    int D = 18;
+//    Date A = new Date(1998, 6, 20);
+//    Date B = new Date(1998, 3, 17);
+//    Date C = new Date(1998, 9, 28);
+//    Date D = new Date(1999, 5, 27);
 }
