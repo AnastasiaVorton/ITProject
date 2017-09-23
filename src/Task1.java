e98dc808ad313275222c4422d452d4c2ad922b5c
Nastya <anastasia.vorton@gmail.com>
2017-09-23 16:58:07 +0300

    Dates added

--- src/Task1.java
+++ src/Task1.java
@@ -5,8 +5,4 @@ import java.util.Date;
  * Class for task number 1
  */
 public class Task1 {
-//    Date A = new Date(1998, 6, 20);
-//    Date B = new Date(1998, 3, 17);
-//    Date C = new Date(1998, 9, 28);
-//    Date D = new Date(1999, 5, 27);
 }
